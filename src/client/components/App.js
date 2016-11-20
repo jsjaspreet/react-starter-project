511dc4250b845a01a60cceec42fa80677dafcb2c
Jaspreet Singh <jaspreet_xps@yahoo.com>
2016-11-19 16:18:05 -0800

    Initial Commit

---
+++ src/client/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    )
+  }
+}
+
+export default App
